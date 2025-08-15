912a3a35946ef371c82437c718302a9dc268d608
lamonte-mi-web <datatimlamonte@gmail.com>
2025-08-15 14:06:07 +0700

    Update

--- src/pages/blogs/[...slug]/index.png.ts
+++ src/pages/blogs/[...slug]/index.png.ts
@@ -5,9 +5,7 @@ import { generateOgImageForPost } from "@/utils/generateOgImages";
 import { SITE } from "@/config";
 
 export async function getStaticPaths() {
-  if (!SITE.dynamicOgImage) {
-    return [];
-  }
+  if (!SITE.dynamicOgImage) return [];
 
   const posts = await getCollection("blog").then(p =>
     p.filter(({ data }) => !data.draft && !data.ogImage)
@@ -21,16 +19,15 @@ export async function getStaticPaths() {
 
 export const GET: APIRoute = async ({ props }) => {
   if (!SITE.dynamicOgImage) {
-    return new Response(null, {
-      status: 404,
-      statusText: "Not found",
-    });
+    return new Response(null, { status: 404, statusText: "Not found" });
   }
 
-  return new Response(
-    await generateOgImageForPost(props as CollectionEntry<"blog">),
-    {
-      headers: { "Content-Type": "image/png" },
-    }
-  );
+  const buffer = await generateOgImageForPost(props as CollectionEntry<"blog">);
+
+  return new Response(new Uint8Array(buffer), {
+    headers: {
+      "Content-Type": "image/png",
+      "Cache-Control": "public, max-age=31536000, immutable"
+    },
+  });
 };
